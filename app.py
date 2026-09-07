d20ccf9e2e1028b38b323dd30f30117e9c11ea9f
Ojasv Kushwah <ojasvkushwah1@gmail.com>
2026-09-08 01:49:01 +0530

    chore: disable uvicorn access logs to prevent console spam

--- app.py
+++ app.py
@@ -255,4 +255,4 @@ def eve():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 8000))
-    uvicorn.run('app:app', host='0.0.0.0', port=port, reload=False)
+    uvicorn.run('app:app', host='0.0.0.0', port=port, reload=False, access_log=False)
